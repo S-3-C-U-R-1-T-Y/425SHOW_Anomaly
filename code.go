2018ac6a53b803222909dd2b04878962f1fff4eb
S3CUR1TY <30468513+S-3-C-U-R-1-T-Y@users.noreply.github.com>
2022-07-26 08:12:13 +0200

    Create code.go

---
+++ code.go
@@ -0,0 +1,4 @@
+"AadClientId": "db032213-d480-4a7d-bfbb-a1337b6822b3",
+"AadSecret": "i_n8Q~6irOx-wNLX~4HvR~YExhMjMxsd7pUGJcZv",
+"AadTenantDomain": "nedscaperlab.onmicrosoft.com",
+"AadTenantId": "316bec54-8d14-4882-8af3-ee5e61570603"
